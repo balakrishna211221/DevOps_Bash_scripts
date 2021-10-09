f35c2d58c8999e53f03d08d2eb7c35601f9fa844
root <root@ip-172-31-94-207.ec2.internal>
2021-10-09 11:06:30 +0000

    dotnet pre req with nginx

---
+++ dotnet_prerequarements.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+apt-get update
+
+apt-get install nginx
+
+wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+
+
+sudo dpkg -i packages-microsoft-prod.deb
+
+
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install dotnet-sdk-2.2
+
+
+#apt-get install dotnet-sdk-3.1
